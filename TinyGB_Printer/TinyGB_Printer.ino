482acbf134772b7bd06a6831d6de3358a737ab11
Dr. Raphaël BOICHOT <raphael.boichot@gmail.com>
2024-12-08 11:15:22 +0100

    Upgrade to 1.0.0

--- TinyGB_Printer/TinyGB_Printer.ino
+++ TinyGB_Printer/TinyGB_Printer.ino
@@ -121,7 +121,7 @@ void setup(void) {
 #endif
 
 #define VERSION_STRING "V3.2.1 (Copyright (C) 2022 Brian KHUU)"
-#define TINY_VERSION_STRING "V0.9 (Copyright (C) 2024 Raphaël BOICHOT)"
+#define TINY_VERSION_STRING "V1.0.0 (Copyright (C) 2024 Raphaël BOICHOT)"
   Serial.println(F("// Game Boy Printer Emulator for Arduino " VERSION_STRING));
   Serial.println(F("// TinyGB Printer converter add-on " TINY_VERSION_STRING));
   Serial.flush();
