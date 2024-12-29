c24d5748ee01a521eb4043b4fff278e70600d734
Dr. Raphaël BOICHOT <raphael.boichot@gmail.com>
2024-12-29 20:48:05 +0100

    Add files via upload

---
+++ TinyGB_Printer/TFT_setup/README.md
@@ -0,0 +1,5 @@
+[https://github.com/Bodmer/TFT_eSPI/issues/3476](https://github.com/Bodmer/TFT_eSPI/issues/3476)
+
+[https://fr.aliexpress.com/item/1005007143117779.html](https://fr.aliexpress.com/item/1005007143117779.html)
+
+[https://github.com/Bodmer/TFT_eSPI](https://github.com/Bodmer/TFT_eSPI)
---
+++ TinyGB_Printer/Tools/Splashscreen_maker.m
@@ -0,0 +1,20 @@
+a=imread('Tear.png');
+
+fid=fopen('splash.h','w');
+[hauteur, largeur, pro]=size(a);
+counter=0;
+
+fprintf(fid,'const unsigned int splashscreen[] = {');
+for i=1:1:hauteur
+   for j=1:1:largeur
+       counter=counter+1;
+       fprintf(fid,'0x');
+       if a(i,j)<=0xF; fprintf(fid,'0');end;
+      fprintf(fid,'%X',a(i,j));
+      fprintf(fid,',');
+      if rem(counter,32)==0;fprintf(fid,'\n'); end;
+   end
+end
+fseek(fid,-2,'cof');
+fprintf(fid,'};');
+fclose(fid);
