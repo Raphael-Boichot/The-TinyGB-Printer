57938216662eec2af7d5e1fa0f47b5130f7e1c0c
Dr. Raphaël BOICHOT <raphael.boichot@gmail.com>
2025-01-21 16:59:03 +0100

    Add files via upload

--- TinyGB_Printer/Tools/Splashscreen_maker.m
+++ TinyGB_Printer/Tools/Splashscreen_maker.m
@@ -1,4 +1,11 @@
-a=imread('Tear.png');
+clc
+clear
+
+disp('-----------------------------------------------------------')
+disp('|      This code can be run with Matlab or Octave         |')
+disp('-----------------------------------------------------------')
+
+a=imread('Splash.png');
 
 fid=fopen('splash.h','w');
 [hauteur, largeur, pro]=size(a);
@@ -6,15 +13,17 @@ counter=0;
 
 fprintf(fid,'const unsigned int splashscreen[] = {');
 for i=1:1:hauteur
-   for j=1:1:largeur
-       counter=counter+1;
-       fprintf(fid,'0x');
-       if a(i,j)<=0xF; fprintf(fid,'0');end;
-      fprintf(fid,'%X',a(i,j));
-      fprintf(fid,',');
-      if rem(counter,32)==0;fprintf(fid,'\n'); end;
-   end
+    for j=1:1:largeur
+        counter=counter+1;
+        fprintf(fid,'0x');
+        if a(i,j)<=0xF; fprintf(fid,'0');end;
+        fprintf(fid,'%X',a(i,j));
+        fprintf(fid,',');
+        if rem(counter,32)==0;fprintf(fid,'\n'); end;
+    end
 end
 fseek(fid,-2,'cof');
 fprintf(fid,'};');
 fclose(fid);
+
+disp('Image file converted to C file')
