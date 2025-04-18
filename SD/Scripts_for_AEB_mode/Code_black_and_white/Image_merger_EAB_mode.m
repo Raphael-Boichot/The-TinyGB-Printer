1dd460102d7d0bd4cafd358e89e9ece0009b7d79
Raphaël BOICHOT <raphael.boichot@gmail.com>
2025-04-18 13:43:58 +0200

    Add files via upload

---
+++ SD/Scripts_for_AEB_mode/Code_black_and_white/Image_merger_EAB_mode.m
@@ -0,0 +1,28 @@
+clc
+clear
+
+disp('-----------------------------------------------------------')
+disp('|Beware, this code is for GNU Octave ONLY !!!             |')
+disp('-----------------------------------------------------------')
+
+pkg load image
+
+%script to be run directly in the image folder
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%User parameters
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+target_png_file='EAB_image.png'; %target file for animated gif
+scaling_factor=2;
+color_limit=4; %threshold to reject an image /255
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+AEB_mode_for_Photo('./Pictures/',target_png_file, color_limit) %processes red average
+
+if not(scaling_factor==1)
+    im=imread(target_png_file);
+    im=imresize(im,scaling_factor,'nearest');
+    imwrite(im,target_png_file);
+end
+
+
+disp('End of conversion, enjoy your fancy RGB fusion !')
