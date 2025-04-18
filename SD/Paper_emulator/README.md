795f8d9df0e5a46afb42546fed8e32737646d163
Raphaël BOICHOT <raphael.boichot@gmail.com>
2025-04-18 13:42:57 +0200

    Add files via upload

---
+++ SD/Paper_emulator/README.md
@@ -0,0 +1,13 @@
+## Some script to simulate thermal paper (requires a PC)
+
+This is a [GNU Octave script](https://octave.org/) to simulate a thermal paper output. GNU Octave is a programming platform / langage for scientific computing, it can be used whatever your prefered OS.
+
+Just drop some images in the **/Image_in** folder, run **Thermal_paper_emulator.m** script and enjoy your images freshly baked in **/Paper_out** folder. You can of course play with the [parameters](https://github.com/Raphael-Boichot/The-TinyGB-Printer/blob/11cf0198a5f711a38a46dcad767c037ce5f8676b/SD/Paper_emulator/Thermal_paper_emulator.m#L9) to modify paper hue and printing intensity.
+
+You can also upload your images to the [GBCamera Android Manager](https://github.com/Mraulio/GBCamera-Android-Manager) and apply the exact same filter.
+
+### Example of starting image
+![alt](/SD/Paper_emulator/Image_in/0001839.png)
+
+### Example of simulated thermal paper (blue hue)
+![alt](/SD/Paper_emulator/Paper_out/printerPaper-dark8-0001839.png)
---
+++ SD/Powershell_script/SetDateTime.ps1
@@ -0,0 +1,46 @@
+# PowerShell Script to Update File Timestamps to Current System Time
+# Script should be placed at the root of the SD card
+
+# Get the current system time
+$currentDateTime = Get-Date
+
+# Automatically get the drive letter of the script's location
+$scriptPath = $MyInvocation.MyCommand.Path
+$driveLetter = [System.IO.Path]::GetPathRoot($scriptPath)
+
+# Ensure we detected the drive letter
+if ($driveLetter) {
+    Write-Host "Detected SD card at drive letter: $driveLetter"
+    
+    # Function to update file timestamps
+    function Update-FileTimestamps {
+        param (
+            [string]$filePath
+        )
+
+        # Set the file timestamps to current system time
+        try {
+            Write-Host "Updating timestamps for: $filePath"
+            Set-ItemProperty -Path $filePath -Name "CreationTime" -Value $currentDateTime
+            Set-ItemProperty -Path $filePath -Name "LastAccessTime" -Value $currentDateTime
+            Set-ItemProperty -Path $filePath -Name "LastWriteTime" -Value $currentDateTime
+        } catch {
+            Write-Host "Failed to update timestamps for $filePath. Error: $_"
+        }
+    }
+
+    # Recursively get all files in the root folder and subfolders
+    $rootFolder = "$driveLetter"  # This is where the script is located (the SD card root)
+    $files = Get-ChildItem -Path $rootFolder -Recurse -File
+
+    # Update timestamps for each file
+    foreach ($file in $files) {
+        Update-FileTimestamps -filePath $file.FullName
+    }
+
+    Write-Host "Timestamps update complete."
+} else {
+    Write-Host "Unable to detect the drive letter. Ensure this script is placed at the root of the SD card."
+}
+
+pause
