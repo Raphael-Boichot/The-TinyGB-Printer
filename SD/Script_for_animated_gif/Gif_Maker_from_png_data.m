ebb936bf713cceb00abee2bb59412c4297fe5665
Raphaël BOICHOT <raphael.boichot@gmail.com>
2025-04-22 16:51:31 +0200

    Add files via upload

---
+++ SD/Script_for_animated_gif/Gif_Maker_from_png_data.m
@@ -0,0 +1,34 @@
+clc
+clear
+
+disp('-----------------------------------------------------------')
+disp('|Beware, this code is for GNU Octave ONLY !!!             |')
+disp('-----------------------------------------------------------')
+
+pkg load image
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%User parameters
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+target_gif_file='Animation.gif'; %target file for animated gif
+gif_deadtime=0.05;               %delay is seconds between pictures for animated gifs
+gif_skip=1;                      %keep every 1 out of gif_skip image for gif
+scaling_factor=1;                %because images are 8x after powershell step
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+listing = dir('./Pictures/*.png');
+for i=1:1:length(listing)
+    name=listing(i).name;
+    disp(['Processing ',listing(i).name]);
+    [frame,map]=imread(['./Pictures/',name]);
+    frame=imresize(frame,scaling_factor,'nearest');
+    if i==1
+        imwrite(frame,map,target_gif_file,'gif', 'Loopcount',inf,'DelayTime',gif_deadtime,'Compression','bzip');
+    else
+        if rem(i,gif_skip)==0
+        imwrite(frame,map,target_gif_file,'gif','WriteMode','append','DelayTime',gif_deadtime,'Compression','bzip');
+        end
+    end
+end
+
+disp('End of conversion, enjoy your fancy animations !')
---
+++ SD/Script_for_animated_gif/README.md
@@ -0,0 +1,10 @@
+## Some script to create animated gif
+
+This is a [GNU Octave scripts](https://octave.org/) to to create animated gif. GNU Octave is a programming platform / langage for scientific computing, it can be used whatever your prefered OS.
+
+Just drop some images taken at different times in the /Pictures folder, run the code script and enjoy your animation !
+
+You can also upload your images to the [GBCamera Android Manager](https://github.com/Mraulio/GBCamera-Android-Manager) and apply the exact same process.
+
+## Example of animated gif
+![](/SD/Script_for_animated_gif/Animation.gif)
